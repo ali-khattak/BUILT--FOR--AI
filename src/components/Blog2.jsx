2de6159b901d5da5d1ae1fe51f79d609a939cfce
Alishan khattak <99354261+ali-khattak@users.noreply.github.com>
2024-07-22 12:55:12 +0500

    Update Blog2.jsx

--- src/components/Blog2.jsx
+++ src/components/Blog2.jsx
@@ -19,13 +19,13 @@ const Blog2 = () => {
         className="w-full h-[300px] md:h-[550px] object-cover rounded-lg"
       />
       <div className="absolute inset-0 flex flex-col justify-end p-5 rounded-lg">
-        <h1 className="text-white font-semibold text-[18px] md:text-[24px]">
+        <h1 className="text-white font-semibold text-[18px] md:text-[24px] p-5">
           {blog.title}
         </h1>
-        <p className="font-normal text-[14px] md:text-[16px] text-gray-200">
+        <p className="font-normal text-[14px] md:text-[16px] text-gray-200 p-5">
           {blog.desc}
         </p>
-        <div className="flex items-center gap-2 md:gap-5 mt-2">
+        <div className="flex items-center gap-2 md:gap-5 mt-2 p-5">
           <p className="font-medium text-[12px] md:text-[14px] text-gray-200">
             {blog.date}
           </p>
